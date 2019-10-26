9fb025b91cbda5cc1ef4d0717937724baeefa9bf
Boris Kondev <boriskondev@gmail.com>
2019-10-26 14:01:57 +0300

    26.10.2019

--- misc.py
+++ misc.py
@@ -1,15 +1,30 @@
-class Party:
-
+class Ball:
     def __init__(self):
-        self.people = []
+        self.position = PVector(width * 0.5, height * 0.5)
+        self.w = 20
+        self.velocity = PVector(5, 5)
+        self.score_player_one = False
+        self.score_player_two = False
 
+    def show(self):
+        fill(255)
+        ellipse(self.position.x, self.position.y, self.w, self.w)
 
-party_1 = Party()
-party_1.people.append("Pesho")
-party_2 = Party()
-party_2.people.append("Vili")
+    def move(self):
+        self.position = self.position.add(self.velocity)
 
-party_2.people.remove("Vili")
+    def border(self):
+        if self.position.x > width:
+            self.velocity.x *= -1
+        elif self.position.x > height:
+            self.velocity.y *= -1
+            self.score_player_one = True
+        elif self.position.x < width:
+            self.velocity.x *= -1
+        elif self.position.x < height:
+            self.velocity.y *= -1
+            self.score_player_two = True
 
-print(party_1.people)
-print(party_2.people)
+    def update(self):
+        self.move()
+        self.border()
