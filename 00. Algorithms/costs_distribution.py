0aa05b62a91b27dc3fa8d51c0be15f7d85fcd814
boriskondev <boriskondev@gmail.com>
2020-01-10 21:07:06 +0200

    10.01.2020

--- 00. Algorithms/costs_distribution.py
+++ 00. Algorithms/costs_distribution.py
@@ -2,20 +2,34 @@ from itertools import combinations
 
 how_many = int(input("How many people are you? "))
 
+print()
+
 people = {}
 
 for i in range(1, how_many + 1):
     name = input(f"Person {i} name: ")
-    expense = float(input(f"Person {i} expense: "))
-    people[name] = expense
-
+    expense = float(input(f"Person {i} expenses: "))
+    people[name] = [expense, 0]
+    print()
 
 for first, second in combinations(people.keys(), 2):
-    first_takes = people[first] / how_many
-    second_takes = people[second] / how_many
+    first_takes = people[first][0] / how_many
+    second_takes = people[second][0] / how_many
     if first_takes > second_takes:
-        print(f"{first} should take {(first_takes - second_takes):.2f} from {second}.")
+        amount_due = first_takes - second_takes
+        print(f"{first} should take {amount_due:.2f} leva from {second}.")
+        people[first][1] += amount_due
     elif second_takes > first_takes:
-        print(f"{second} should take {(second_takes - first_takes):.2f} from {first}.")
+        amount_due = second_takes - first_takes
+        print(f"{second} should take {amount_due:.2f} leva from {first}.")
+        people[second][1] += amount_due
     else:
         print(f"{first} and {second} are clear. No one owes anything.")
+
+print()
+
+print("At the end of the day the situation is:")
+for name, details in sorted(people.items(), key=lambda x: x[1][1]):
+    print(f"- {name} should receive {details[1]:.2f} leva in total")
+
+# Better mechanics here - not only income, but expense as well?
