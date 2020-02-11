e98d7caf7492cc503f892c1322efe9bb681b9717
boriskondev <boriskondev@gmail.com>
2020-02-11 13:03:40 +0200

    11.02.2020

--- 02. Advanced module/00. Exams/Santa's Present Factory.py
+++ 02. Advanced module/00. Exams/Santa's Present Factory.py
@@ -1,7 +1,5 @@
 from collections import deque
 
-traverse = 0
-materials_to_add = 0
 crafted = {}
 
 magic_and_presents = {150: "Doll",
@@ -15,8 +13,10 @@ magic_values_queue = deque(map(int, input().split()))
 if len(materials_stack) == len(magic_values_queue):
     traverse = len(materials_stack)
 
-for case in range(traverse):
-    material = materials_stack[-1] + materials_to_add
+while True:
+    if not materials_stack or not magic_values_queue:
+        break
+    material = materials_stack[-1]
     magic = magic_values_queue[0]
     currently_crafted = material * magic
     if currently_crafted in magic_and_presents:
@@ -27,12 +27,12 @@ for case in range(traverse):
         else:
             crafted[magic_and_presents[currently_crafted]] = 1
     elif currently_crafted < 0:
-        materials_to_add = material + magic
         materials_stack.pop()
+        materials_stack.append(material + magic)
         magic_values_queue.popleft()
     elif currently_crafted > 0:
         magic_values_queue.popleft()
-        materials_to_add = 15
+        materials_stack[-1] += 15
     elif material == 0 and magic == 0:
         materials_stack.pop()
         magic_values_queue.popleft()
@@ -41,4 +41,15 @@ for case in range(traverse):
     elif magic == 0:
         magic_values_queue.popleft()
 
-print(crafted)
+if "Doll" in crafted and "Wooden train" in crafted or "Teddy bear" in crafted and "Bicycle" in crafted:
+    print("The presents are crafted! Merry Christmas!")
+else:
+    print("No presents this Christmas!")
+
+if materials_stack:
+    print("Materials left: " + ', '.join([str(x) for x in materials_stack[::-1]]))
+if magic_values_queue:
+    print("magic left: " + ', '.join([str(x) for x in magic_values_queue]))
+
+for item, quantity in sorted(crafted.items()):
+    print(f"{item}: {quantity}")
