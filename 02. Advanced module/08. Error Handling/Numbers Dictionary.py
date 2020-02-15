947af60e4c86d88dfa32a53ecbfbf63e13cff6db
boriskondev <boriskondev@gmail.com>
2020-02-15 13:30:35 +0200

    15.02.2020

--- 02. Advanced module/08. Error Handling/Numbers Dictionary.py
+++ 02. Advanced module/08. Error Handling/Numbers Dictionary.py
@@ -1,3 +1,44 @@
+# I solved the task in two different ways.
+# The first one is according to the exercise - with one try and many except clauses.
+# The second one (commented) is following the original code and it is a bit ugly,
+# but I left it anyway as it was my initial solution.
+
+# Solution 1
+
+numbers_dictionary = {}
+
+while True:
+    line = input()
+    try:
+        if line == "End":
+            break
+        elif line == "Search":
+            searched = input()
+            if searched == "Remove":
+                searched = input()
+                if searched == "End":
+                    break
+                else:
+                    del numbers_dictionary[searched]
+            else:
+                print(numbers_dictionary[searched])
+        elif line == "Remove":
+            searched = input()
+            del numbers_dictionary[searched]
+        else:
+            number_as_string = line
+            number = int(input())
+            numbers_dictionary[number_as_string] = number
+    except ValueError:
+        print("The variable number must be an integer")
+    except KeyError:
+        print("Number does not exist in dictionary")
+
+print(numbers_dictionary)
+
+# Solution 2
+
+'''
 numbers_dictionary = {}
 
 line = input()
@@ -32,3 +73,4 @@ while line != "End":
     line = input()
 
 print(numbers_dictionary)
+'''
