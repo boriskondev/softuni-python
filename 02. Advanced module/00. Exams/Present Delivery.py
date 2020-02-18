9aee1fafebee03bc8ea99c0c5931ceae0a06350a
boriskondev <boriskondev@gmail.com>
2020-02-18 13:40:48 +0200

    16.02.2020

--- 02. Advanced module/00. Exams/Present Delivery.py
+++ 02. Advanced module/00. Exams/Present Delivery.py
@@ -1,10 +1,3 @@
-matrix = []
-santa_position = ()
-nice_kids = 0
-nice_kids_left = 0
-no_more_gifts = False
-
-
 def print_matrix(array):
     for r in array:
         print(" ".join(r))
@@ -28,130 +21,90 @@ def check_around(r, c, array, gifts, good_kids):
         array[r][c] = "-"
     if gifts == 0:
         no_more_gifts = True
-        return r, c, array, gifts, good_kids
+        return r, c, array, gifts, good_kids, no_more_gifts
     return r, c, array, gifts, good_kids
 
 
-def award_everyone(matrix_row, matrix_col, array, gifts, good_kids):
-    if check_index(matrix_row - 1, matrix_col - 1, array):
-        check_around(matrix_row - 1, matrix_col - 1, array, gifts, good_kids)
+def award_everyone(new_row, new_col, array, gifts, good_kids):
+    if check_index(new_row - 1, new_col - 1, array):
+        check_around(new_row - 1, new_col - 1, array, gifts, good_kids)
         print_matrix(array)
         print()
-    if check_index(matrix_row - 1, matrix_col, array):
-        check_around(matrix_row - 1, matrix_col, array, gifts, good_kids)
+    if check_index(new_row - 1, new_col, array):
+        check_around(new_row - 1, new_col, array, gifts, good_kids)
         print_matrix(array)
         print()
-    if check_index(matrix_row - 1, matrix_col + 1, array):
-        check_around(matrix_row - 1, matrix_col + 1, array, gifts, good_kids)
+    if check_index(new_row - 1, new_col + 1, array):
+        check_around(new_row - 1, new_col + 1, array, gifts, good_kids)
         print_matrix(array)
         print()
-    if check_index(matrix_row, matrix_col + 1, array):
-        check_around(matrix_row, matrix_col + 1, array, gifts, good_kids)
+    if check_index(new_row, new_col + 1, array):
+        check_around(new_row, new_col + 1, array, gifts, good_kids)
         print_matrix(array)
         print()
-    if check_index(matrix_row + 1, matrix_col + 1, array):
-        check_around(matrix_row + 1, matrix_col + 1, array, gifts, good_kids)
+    if check_index(new_row + 1, new_col + 1, array):
+        check_around(new_row + 1, new_col + 1, array, gifts, good_kids)
         print_matrix(array)
         print()
-    if check_index(matrix_row + 1, matrix_col, array):
-        check_around(matrix_row + 1, matrix_col, array, gifts, good_kids)
+    if check_index(new_row + 1, new_col, array):
+        check_around(new_row + 1, new_col, array, gifts, good_kids)
         print_matrix(array)
         print()
-    if check_index(matrix_row + 1, matrix_col - 1, array):
-        check_around(matrix_row + 1, matrix_col - 1, array, gifts, good_kids)
+    if check_index(new_row + 1, new_col - 1, array):
+        check_around(new_row + 1, new_col - 1, array, gifts, good_kids)
         print_matrix(array)
         print()
-    if check_index(matrix_row, matrix_col - 1, array):
-        check_around(matrix_row, matrix_col - 1, array, gifts, good_kids)
+    if check_index(new_row, new_col - 1, array):
+        check_around(new_row, new_col - 1, array, gifts, good_kids)
         print_matrix(array)
         print()
     print_matrix(array)
     print()
-    return matrix_row, matrix_col, array, gifts, good_kids
+    return new_row, new_col, array, gifts, good_kids
 
 
-def move_up(matrix_row, matrix_col, array, gifts, good_kids):
-    if check_index(matrix_row - 1, matrix_col, array):
-        if array[matrix_row - 1][matrix_col] == "V":
-            gifts -= 1
-            good_kids -= 1
-            array[matrix_row - 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row - 1][matrix_col] == "X":
-            array[matrix_row - 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row - 1][matrix_col] == "C":
-            array[matrix_row - 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-            award_everyone(matrix_row, matrix_col, array, gifts, good_kids)
-        elif array[matrix_row - 1][matrix_col] == "-":
-            array[matrix_row - 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-        print_matrix(array)
-    return matrix_row, matrix_col, array, gifts, good_kids
+def take_move(old_row, old_col, new_row, new_col, array, gifts, good_kids):
+    if array[new_row][new_col] == "V":
+        gifts -= 1
+        good_kids -= 1
+        array[new_row][new_col] = "S"
+        array[old_row][old_col] = "-"
+    elif array[new_row][new_col] == "X":
+        array[new_row][new_col] = "S"
+        array[old_row][old_col] = "-"
+    elif array[new_row][new_col] == "C":
+        array[new_row][new_col] = "S"
+        array[old_row][old_col] = "-"
+        award_everyone(new_row, new_col, array, gifts, good_kids)
+    elif array[new_row][new_col] == "-":
+        array[new_row][new_col] = "S"
+        array[old_row][old_col] = "-"
+    print_matrix(array)
+    position = new_row, new_col
+    return position, array, gifts, good_kids
 
 
-def move_right(matrix_row, matrix_col, array, gifts, good_kids):
-    if check_index(matrix_row, matrix_col + 1, array):
-        if array[matrix_row][matrix_col + 1] == "V":
-            gifts -= 1
-            good_kids -= 1
-            array[matrix_row][matrix_col + 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row][matrix_col + 1] == "X":
-            array[matrix_row][matrix_col + 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row][matrix_col + 1] == "C":
-            array[matrix_row][matrix_col + 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-            award_everyone(matrix_row, matrix_col, array, gifts, good_kids)
-        elif array[matrix_row][matrix_col + 1] == "-":
-            array[matrix_row][matrix_col + 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-        print_matrix(array)
-    return matrix_row, matrix_col, array, gifts, good_kids
+def prepare_for_action(com, position, array, gifts, good_kids):
+    old_row, old_col = position[0], position[1]
+    new_row, new_col = None, None
+    if com == "up":
+        new_row, new_col = position[0] - 1, position[1]
+    elif com == "right":
+        new_row, new_col = position[0], position[1] + 1
+    elif com == "down":
+        new_row, new_col = position[0] + 1, position[1]
+    elif com == "left":
+        new_row, new_col = position[0], position[1] - 1
+    if check_index(new_row, new_col, array):
+        position, array, gifts, good_kids = take_move(old_row, old_col, new_row, new_col, array, gifts, good_kids)
+        return position, array, gifts, good_kids
 
 
-def move_down(matrix_row, matrix_col, array, gifts, good_kids):
-    if check_index(matrix_row + 1, matrix_col, array):
-        if array[matrix_row + 1][matrix_col] == "V":
-            gifts -= 1
-            good_kids -= 1
-            array[matrix_row + 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row + 1][matrix_col] == "X":
-            array[matrix_row + 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row + 1][matrix_col] == "C":
-            array[matrix_row + 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-            award_everyone(matrix_row, matrix_col, array, gifts, good_kids)
-        elif array[matrix_row + 1][matrix_col] == "-":
-            array[matrix_row + 1][matrix_col] = "S"
-            array[matrix_row][matrix_col] = "-"
-        print_matrix(array)
-    return matrix_row, matrix_col, array, gifts, good_kids
-
-
-def move_left(matrix_row, matrix_col, array, gifts, good_kids):
-    if check_index(matrix_row, matrix_col - 1, array):
-        if array[matrix_row][matrix_col - 1] == "V":
-            gifts -= 1
-            good_kids -= 1
-            array[matrix_row][matrix_col - 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row][matrix_col - 1] == "X":
-            array[matrix_row][matrix_col - 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-        elif array[matrix_row][matrix_col - 1] == "C":
-            array[matrix_row][matrix_col - 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-            award_everyone(matrix_row, matrix_col, array, gifts, good_kids)
-        elif array[matrix_row][matrix_col - 1] == "-":
-            array[matrix_row][matrix_col - 1] = "S"
-            array[matrix_row][matrix_col] = "-"
-        print_matrix(array)
-    return matrix_row, matrix_col, array, gifts, good_kids
+matrix = []
+santa_position = ()
+nice_kids = 0
+nice_kids_left = 0
+no_more_gifts = False
 
 
 count_of_presents = int(input())
@@ -173,21 +126,12 @@ while True:
     command = input()
     if command == "Christmas morning":
         break
-    elif command == "up":
-        move_up(santa_position[0], santa_position[1], matrix, count_of_presents, nice_kids)
-    elif command == "down":
-        move_down(santa_position[0], santa_position[1], matrix, count_of_presents, nice_kids)
-    elif command == "left":
-        move_left(santa_position[0], santa_position[1], matrix, count_of_presents, nice_kids)
-    elif command == "right":
-        move_right(santa_position[0], santa_position[1], matrix, count_of_presents, nice_kids)
+    else:
+        santa_position, matrix, count_of_presents, nice_kids = \
+            prepare_for_action(command, santa_position, matrix, count_of_presents,nice_kids)
     if count_of_presents == 0:
         print("Santa ran out of presents!")
         break
-    for row in range(len(matrix)):
-        for col in range(len(matrix)):
-            if matrix[row][col] == "S":
-                santa_position = row, col
 
 for row in range(len(matrix)):
     for col in range(len(matrix)):
@@ -197,6 +141,6 @@ for row in range(len(matrix)):
 print_matrix(matrix)
 
 if nice_kids_left == 0 and count_of_presents >= 0:
-    print(f"Good job, Santa! {nice_kids - nice_kids_left} happy nice kid/s.")
+    print(f"Good job, Santa! {nice_kids_left} happy nice kid/s.")
 else:
     print(f"No presents for {nice_kids_left} nice kid/s.")
