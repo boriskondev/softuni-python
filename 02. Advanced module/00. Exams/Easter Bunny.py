dc605816d57634dcb0702004f81946a08f6a6608
pmg23_b.kondev <boriskondev@gmail.com>
2020-04-08 21:39:15 +0300

    08.04.2020

--- 02. Advanced module/00. Exams/Easter Bunny.py
+++ 02. Advanced module/00. Exams/Easter Bunny.py
@@ -1,17 +1,6 @@
-def check_index(matrix_row, matrix_col, array):
-    if 0 <= matrix_row < len(array) and 0 <= matrix_col < len(array):
-        return True
-    return False
-
-
 matrix = []
-best_result = 0
-best_direction = ""
-best_move_coords = []
 
-field_size = int(input())
-
-for _ in range(field_size):
+for _ in range(int(input())):
     matrix.append(input().split(" "))
 
 bunny_position = 0
@@ -23,137 +12,50 @@ for row in range(len(matrix)):
 
 original_position = bunny_position
 
-cannot_move = False
-bunny_position = original_position
-current_result = 0
-current_move_coords = []
+best_result = 0
+best_direction = ""
+best_move_coords = []
+cannot_move = 0
 
-# UP
-while True:
-    if cannot_move:
-        break
-    current_row, current_col = bunny_position[0], bunny_position[1]
-    desired_row = current_row - 1
-    desired_col = current_col
-    if check_index(desired_row, desired_col, matrix):
-        if matrix[desired_row][desired_col] != "X":
-            current_result += int(matrix[desired_row][desired_col])
-            current_move_coords.append([current_row, current_col])
-            bunny_position = desired_row, desired_col
+directions = ["up", "right", "down", "left"]
+
+for direction in directions:
+    cannot_move = False
+    bunny_position = original_position
+    current_result = 0
+    current_move_coords = []
+    while True:
+        if cannot_move:
+            break
+        current_row, current_col = bunny_position[0], bunny_position[1]
+        if direction == "up":
+            desired_row, desired_col = current_row - 1, current_col
+        elif direction == "right":
+            desired_row, desired_col = current_row, current_col + 1
+        elif direction == "down":
+            desired_row, desired_col = current_row + 1, current_col
+        else:
+            desired_row, desired_col = current_row, current_col - 1
+        if 0 <= desired_row < len(matrix) and 0 <= desired_col < len(matrix):
+            if matrix[desired_row][desired_col] != "X":
+                current_result += int(matrix[desired_row][desired_col])
+                current_move_coords.append([current_row, current_col])
+                bunny_position = desired_row, desired_col
+            else:
+                current_move_coords.append([current_row, current_col])
+                cannot_move = True
+                break
         else:
             current_move_coords.append([current_row, current_col])
             cannot_move = True
             break
-    else:
-        current_move_coords.append([current_row, current_col])
-        cannot_move = True
-        break
+        if current_result > best_result:
+            best_result = current_result
+            best_move_coords = current_move_coords
+            best_direction = direction
 
-if current_result > best_result:
-    best_result = current_result
-    best_move_coords = current_move_coords
-    best_direction = "up"
-
-cannot_move = False
-bunny_position = original_position
-current_result = 0
-current_move_coords = []
-
-# DOWN
-while True:
-    if cannot_move:
-        break
-    current_row, current_col = bunny_position[0], bunny_position[1]
-    desired_row = current_row + 1
-    desired_col = current_col
-    if check_index(desired_row, desired_col, matrix):
-        if matrix[desired_row][desired_col] != "X":
-            current_result += int(matrix[desired_row][desired_col])
-            current_move_coords.append([current_row, current_col])
-            bunny_position = desired_row, desired_col
-        else:
-            current_move_coords.append([current_row, current_col])
-            cannot_move = True
-            break
-    else:
-        current_move_coords.append([current_row, current_col])
-        cannot_move = True
-        break
-
-if current_result > best_result:
-    best_result = current_result
-    best_move_coords = current_move_coords
-    best_direction = "down"
-
-cannot_move = False
-bunny_position = original_position
-current_result = 0
-current_move_coords = []
-
-# LEFT
-while True:
-    if cannot_move:
-        break
-    current_row, current_col = bunny_position[0], bunny_position[1]
-    desired_row = current_row
-    desired_col = current_col - 1
-    if check_index(desired_row, desired_col, matrix):
-        if matrix[desired_row][desired_col] != "X":
-            current_result += int(matrix[desired_row][desired_col])
-            current_move_coords.append([current_row, current_col])
-            bunny_position = desired_row, desired_col
-        else:
-            current_move_coords.append([current_row, current_col])
-            cannot_move = True
-            break
-    else:
-        current_move_coords.append([current_row, current_col])
-        cannot_move = True
-        break
-
-if current_result > best_result:
-    best_result = current_result
-    best_move_coords = current_move_coords
-    best_direction = "left"
-
-cannot_move = False
-bunny_position = original_position
-current_result = 0
-current_move_coords = []
-
-# RIGHT
-while True:
-    if cannot_move:
-        break
-    current_row, current_col = bunny_position[0], bunny_position[1]
-    desired_row = current_row
-    desired_col = current_col + 1
-    if check_index(desired_row, desired_col, matrix):
-        if matrix[desired_row][desired_col] != "X":
-            current_result += int(matrix[desired_row][desired_col])
-            current_move_coords.append([current_row, current_col])
-            bunny_position = desired_row, desired_col
-        else:
-            current_move_coords.append([current_row, current_col])
-            cannot_move = True
-            break
-    else:
-        current_move_coords.append([current_row, current_col])
-        cannot_move = True
-        break
-
-if current_result > best_result:
-    best_result = current_result
-    best_move_coords = current_move_coords
-    best_direction = "right"
-
-cannot_move = False
-bunny_position = original_position
-current_result = 0
-current_move_coords = []
 
 print(best_direction)
 for coords in best_move_coords[1:]:
     print(coords)
 print(best_result)
-
